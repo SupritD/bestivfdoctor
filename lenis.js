6ecdaf4d8f273621a56e3262a8fc660dc2fb4462
suprit-infinity-plus1 <suprit@infinityplus1.in>
2026-01-13 13:55:38 +0530

    first commit

---
+++ lenis.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const lenis = new Lenis({
+    duration: 1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smoothWheel: true,
+    smoothTouch: false,
+    wheelMultiplier: 0.5,
+    touchMultiplier: 2,
+    infinite: false,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  // DEBUG (remove later)
+  // lenis.on("scroll", ({ velocity }) => {
+  //   console.log("velocity:", velocity);
+  // });
+});
